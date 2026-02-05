66988589c8f8de5abe900cf70981bbf151d44a12
Bruno Salles <brunobr2603salles@gmail.com>
2026-02-05 20:20:54 -0300

    feat: Criação dos controllers definindo rotas e ultilizando as annotatios para definir as rotas

--- src/main/java/dev/java10x/CadastroDeUsuarios/Cadastro/Controller/UsuariosController.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Cadastro/Controller/UsuariosController.java
@@ -1,4 +1,17 @@
 package dev.java10x.CadastroDeUsuarios.Cadastro.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class UsuariosController {
+
+       @GetMapping(name = "/Boas Vindas")
+       public String boasVindas(){
+           return "Boas Vindas";
+       }
+
+
 }
--- src/main/java/dev/java10x/CadastroDeUsuarios/DadosBancarios/Controller/BancoController.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/DadosBancarios/Controller/BancoController.java
@@ -1,4 +1,16 @@
 package dev.java10x.CadastroDeUsuarios.DadosBancarios.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class BancoController {
+
+    @GetMapping("/Banco")
+    public String banco(){
+        return "este e meu banco";
+    }
+
 }
